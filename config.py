7fd85aec6d940341e5cc772747d9465edd48d48f
shahbazjutt5535-web <shahbazjutt5535@gmail.com>
2026-06-26 22:57:56 +0300

    Add configuration settings in config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+BARS = 800
+
+DEFAULT_EXCHANGE = "PSX"
+
+TIMEFRAMES = {
+    "5m": "5m",
+    "15m": "15m",
+    "30m": "30m",
+    "1h": "1h",
+    "4h": "4h",
+    "1d": "1d",
+    "1w": "1w"
+}
