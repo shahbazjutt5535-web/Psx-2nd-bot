4974726324597820ff8428d5b7afda4579ab935c
shahbazjutt5535-web <shahbazjutt5535@gmail.com>
2026-07-09 13:14:26 +0300

    Fix UREA symbol in bot.py

--- bot.py
+++ bot.py
@@ -140,7 +140,7 @@ SYMBOLS = {
     "NBPGETF": ("NBPGETF", "PSX"),
     "KSE100": ("KSE100", "PSX"),
     "NATURALGAS": ("NATURALGAS", "CAPITALCOM"),
-    "UREA": ("UFV1", "CBOT"),
+    "UREA": ("UFV1!", "CBOT"),
     "USDPKR": ("USDPKR", "FX_IDC"),
 
     # ================= FOREX =================
